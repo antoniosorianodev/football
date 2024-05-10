09e332d71b90f9758d13209d9a81ff9abc1a9e25
Antonio <asoriano@appdev.yearup.org>
2024-05-10 15:54:10 -0500

    added code to build dropdown

--- scripts/index.js
+++ scripts/index.js
@@ -1,2 +1,37 @@
 "use strict"
 
+// these are our teams
+let teams = [
+    { code: "DAL", name: "Dallas Cowboys", plays: "Arlington, TX" },
+    { code: "DEN", name: "Denver Broncos", plays: "Denver, CO" },
+    { code: "HOU", name: "Houston Texans", plays: "Houston, TX" },
+    { code: "KAN", name: "Kansas City Chiefs", plays: "Kansas City, MO" },
+];
+
+// this is our window.onload function that runs code once the dom is loaded
+window.onload = function () {
+    initDropdown();
+};
+
+function initDropdown() {
+
+    // get the dropdown from the HTML document and assign it to a variable
+    let theDropdown = document.querySelector("#footballSelect");
+
+    // get the total number of teams we have for use in the loop
+    let numberOfTeams = teams.length;
+
+    for (let i = 0; i < numberOfTeams; i++) {
+
+        // lets create a new option
+        let newOption = document.createElement("option");
+
+        // set the text content for our new option, what our user sees
+        newOption.textContent = teams[i].name;
+
+        // set the value for the option
+        newOption.value = teams[i].code;
+
+        theDropdown.appendChild(newOption);
+    }
+}
